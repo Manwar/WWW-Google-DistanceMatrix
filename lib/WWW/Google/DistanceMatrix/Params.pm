f5fc022069121cc916172d1901375825abf7629b
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-01-18 18:04:09 +0000

    - Added key 'provides' to the Makefile.PL script.

--- lib/WWW/Google/DistanceMatrix/Params.pm
+++ lib/WWW/Google/DistanceMatrix/Params.pm
@@ -1,6 +1,6 @@
 package WWW::Google::DistanceMatrix::Params;
 
-$WWW::Google::DistanceMatrix::Params::VERSION   = '0.16';
+$WWW::Google::DistanceMatrix::Params::VERSION   = '0.17';
 $WWW::Google::DistanceMatrix::Params::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ WWW::Google::DistanceMatrix::Params - Placeholder for parameters for WWW::Google
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 =cut
 
